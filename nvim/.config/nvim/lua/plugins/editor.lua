c08bfc6b8bfc77a76262de920fbe8476e84f3d79
Gustavo Basso <gubasso@eambar.net>
2025-01-19 14:15:22 -0300

    cleaning unused plugin auto-save

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,16 +1,4 @@
 return {
-  -- {
-  --   'okuuva/auto-save.nvim',
-  --   opts = {
-  --     write_all_buffers = true,
-  --     debounce_delay = 10000,
-  --     trigger_events = { -- See :h events
-  --       immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
-  --       defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
-  --       cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
-  --     },
-  --   },
-  -- },
   { 'jiangmiao/auto-pairs' },
   {
     'stevearc/oil.nvim',
