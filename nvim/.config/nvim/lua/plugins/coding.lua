b8c25e13bc1dd20aead32f3737b621ba9752c362
Gustavo Basso <gustavo.basso@suse.com>
2025-01-20 23:35:42 -0300

    add git keybinging

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -290,6 +290,7 @@ return {
   {
     "tpope/vim-fugitive",
     keys = {
+      { "<leader>gg", ":Git ", desc = "Git Command" },
       { "gs", ":Git<CR>", desc = "Git Status" },
       { "<leader>gs", ":Git<CR>", desc = "Git Status" },
       { "<leader>gp", ":Git push<CR>", desc = "Git Push" },
