c8b2bb602c75299edf9599714ab8c13b8216dff0
Gustavo Basso <gustavo.basso@suse.com>
2025-01-20 23:22:43 -0300

    env vars by host

---
+++ shell/.config/shell/env_vars/common.sh
@@ -0,0 +1,68 @@
+#########################
+# Environment Variables #
+#########################
+
+# fzf
+## vars
+skip=".git,node_modules,target"
+bat_cmd="bat -n --color=always"
+tree_cmd='eza --tree --all --git-ignore --ignore-glob ".git" --color=always'
+preview="($bat_cmd {} 2> /dev/null || $tree_cmd {} 2> /dev/null) | head -200"
+
+
+## Defaults
+export FZF_DEFAULT_COMMAND="fd --hidden --follow --color=always"
+export FZF_DEFAULT_OPTS="
+  --select-1 --exit-0
+  --walker-skip $skip
+  --no-height
+  --layout=reverse
+  --ansi
+  --inline-info"
+export FZF_TMUX=1
+## CTRL_T
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_CTRL_T_OPTS="
+  --preview '$preview'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+## ALT_C
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type d"
+export FZF_ALT_C_OPTS="
+  --preview '$tree_cmd {} | head -200'"
+## CTRL_R
+# bat_shell="bat --language sh --plain --color=always"
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window down:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --bind 'ctrl-y:execute-silent(echo -n {} | xclip -selection clipboard)+abort'
+  --color header:italic
+  --header 'Press ^y to copy command into clipboard'"
+## COMPLETIONS **
+export FZF_COMPLETION_OPTS="
+  --preview '$preview'
+  --info=inline"
+_fzf_compgen_path() {
+  fd --hidden --follow --color=always . "$1"
+}
+
+_fzf_compgen_dir() {
+  fd --hidden --follow --color=always --type d . "$1"
+}
+
+export DOTFILESDIR="${HOME}/.dotfiles"
+export VISUAL=nvim
+export EDITOR=nvim
+export TERMINAL=wezterm
+
+export R_PROFILE_USER="$XDG_CONFIG_HOME/R"
+export R_HOME_USER="$HOME/.R"
+
+export GOPATH="$HOME/.go"
+export GOBIN="$GOPATH/bin"
+
+export CLOUD_DIR="$HOME/Nextcloud"
+
+SOLANA_PATH="$XDG_DATA_HOME/solana/install/active_release/"
+
+export PATH="$PATH:$GOBIN:$SOLANA_PATH"
+export PATH="$PATH:$HOME/.local/bin:$HOME/.cargo/bin"
---
+++ shell/.config/shell/env_vars/tumblesuse.sh
@@ -0,0 +1,2 @@
+export SSH_ASKPASS=/usr/libexec/ssh/ksshaskpass
+export SSH_ASKPASS_REQUIRE=prefer
