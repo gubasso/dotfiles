1fdb23919e38969a2d1257685fdb4eeb5b75bab3
Gustavo Basso <gustavo.basso@suse.com>
2025-01-23 08:52:28 -0300

    shell/alias suse full system update

--- shell/.config/shell/aliases/tumblesuse.sh
+++ shell/.config/shell/aliases/tumblesuse.sh
@@ -1,1 +1,13 @@
 alias clip='xclip -selection clipboard'
+function up() {
+  install_aws_cli_v2
+  flatpak update
+  rustup update
+  cargo install-update -a
+  pipx upgrade-all
+  sudo zypper refresh
+  sudo zypper up -y
+  sudo zypper dup
+  # sudo fwupdmgr refresh
+  # sudo fwupdmgr update --assume-yes
+;}
