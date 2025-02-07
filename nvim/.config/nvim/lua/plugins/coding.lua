0a13da23703594311e6caaf34d35e2ee77630996
Gustavo Basso <gustavo.basso@suse.com>
2025-02-07 09:38:26 -0300

    nvim: gitconflict key mappings

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -301,7 +301,20 @@ return {
   {
     'akinsho/git-conflict.nvim',
     version = "*",
-    config = true
+    keys = {
+      { "<leader>co", "<cmd>GitConflictChooseOurs<CR>", desc = "Choose Ours" },
+      { "<leader>ct", "<cmd>GitConflictChooseTheirs<CR>", desc = "Choose Theirs" },
+      { "<leader>cb", "<cmd>GitConflictChooseBoth<CR>", desc = "Choose Both" },
+      { "<leader>c0", "<cmd>GitConflictChooseNone<CR>", desc = "Choose None" },
+      { "]x", "<cmd>GitConflictNextConflict<CR>", desc = "Next Conflict" },
+      { "[x", "<cmd>GitConflictPrevConflict<CR>", desc = "Previous Conflict" },
+      { "<leader>cl", "<cmd>GitConflictListQf<CR>", desc = "List Conflicts in Quickfix" },
+    },
+    config = function()
+      require('git-conflict').setup({
+        default_mappings = false, -- Disable default mappings
+      })
+    end,
   },
   { "olrtg/nvim-emmet" },
 }
