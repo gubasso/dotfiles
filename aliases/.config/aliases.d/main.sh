26af6d4d3157b4fb98257102a696b3f2175e45cd
Gustavo Basso <gustavo.basso@suse.com>
2025-06-10 23:27:29 -0300

    loading private scripts

--- aliases/.config/aliases.d/main.sh
+++ aliases/.config/aliases.d/main.sh
@@ -7,9 +7,11 @@ source "$ALIASES_DIR/general.sh"
 hostname=$(uname -n)
 case "$hostname" in
   valinor)
-    source "$ALIASES_DIR"/host-valinor.sh
+    source "$ALIASES_DIR/host-valinor.sh"
     ;;
   tumblesuse)
-    source "$ALIASES_DIR"/host-tumblesuse.sh
+    source "$ALIASES_DIR/host-tumblesuse.sh"
     ;;
 esac
+
+source "$ALIASES_DIR/private/main.sh"
--- env/.config/env.d/main.sh
+++ env/.config/env.d/main.sh
@@ -4,14 +4,16 @@
 
 export ENV_DIR="$XDG_CONFIG_HOME/env.d"
 
-source "$ENV_DIR"/general.sh
+source "$ENV_DIR/general.sh"
 
 hostname=$(uname -n)
 case "$hostname" in
   valinor)
-    source "$ENV_DIR"/host-valinor.sh
+    source "$ENV_DIR/host-valinor.sh"
     ;;
   tumblesuse)
-    source "$ENV_DIR"/host-tumblesuse.sh
+    source "$ENV_DIR/host-tumblesuse.sh"
     ;;
 esac
+
+source "$ENV_DIR/private/main.sh"
