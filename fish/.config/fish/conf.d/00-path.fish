eb5ac5a4cd3cd416153a342e70176f5dfc9d15ab
Gustavo Basso <gustavo.basso@suse.com>
2025-12-18 12:22:09 -0300

    fish path

---
+++ fish/.config/fish/conf.d/00-path.fish
@@ -0,0 +1,7 @@
+# Keep fish PATH consistent across host and containers.
+# fish_add_path ignores missing dirs and avoids duplicates.
+# "duplicates/mirror" ~/.profile
+
+fish_add_path -m ~/.local/bin
+fish_add_path -m ~/.cargo/bin
+fish_add_path -m ~/.npm-global/bin
