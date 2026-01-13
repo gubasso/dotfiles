bb041f7d2fc58e939c866712ac1f32bf7e75c4fc
Gustavo Basso <gustavo.basso@suse.com>
2026-01-13 12:22:18 -0300

    nvim yt_get_reference

--- nvim/.config/nvim/lua/core/usercmds/init.lua
+++ nvim/.config/nvim/lua/core/usercmds/init.lua
@@ -1,7 +1,7 @@
 -- User commands module loader
 -- Loads all user command sub-modules
 
-require("core.usercmds.ytmdlink")
+require("core.usercmds.yt_get_reference")
 require("core.usercmds.copy_file_path")
 require("core.usercmds.md_new").setup()
 require("core.usercmds.deps")
