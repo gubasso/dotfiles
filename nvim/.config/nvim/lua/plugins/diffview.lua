75cb4fdd4ada8403fbad82c2e4c6a4d8693b325c
Gustavo Basso <gustavo.basso@suse.com>
2026-01-18 00:30:46 -0300

    nvim

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,4 +1,8 @@
 return {
   "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>gD", "<cmd>DiffviewOpen<CR>", desc = "Diffview Open" },
+    { "<leader>gq", "<cmd>DiffviewClose<CR>", desc = "Diffview Close" },
+  },
   opts = { view = { merge_tool = { layout = "diff4_mixed" } } },
 }
